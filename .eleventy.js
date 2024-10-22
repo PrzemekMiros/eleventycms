50ccbcf8b02f010b28bfcf711b22de4d1ac0cad9
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-10-22 21:09:09 +0200

    Check11

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/css/"); 
     eleventyConfig.addPassthroughCopy("src/assets/img/"); 
     eleventyConfig.addPassthroughCopy("src/admin"); 
+    eleventyConfig.addPassthroughCopy("users.json"); 
     
     return {
         dir: {
