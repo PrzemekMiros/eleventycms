6f475d61f3c669fc5106eee2391c63473c079409
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-10-24 18:05:22 +0200

    Check16

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,19 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/assets/css/"); 
     eleventyConfig.addPassthroughCopy("src/assets/img/"); 
-    eleventyConfig.addPassthroughCopy("src/admin"); 
-    eleventyConfig.addPassthroughCopy("users.json"); 
+    eleventyConfig.addPassthroughCopy("src/content/"); 
+
+    // Collections blog
+    eleventyConfig.addCollection('posts', function(collectionApi) {
+        return collectionApi.getFilteredByGlob('src/content/posts/**/*.md').reverse();
+    });
+        
     
     return {
         dir: {
             input: "src",
             includes: "includes",
-            output: "public"
+            output: "public" 
         }
     };
 };
